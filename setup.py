5ab4497eb3dfb0359d5c51a46750092e7a4e8e70
Andrew Lensen <andrew.lensen@ecs.vuw.ac.nz>
2020-04-06 14:19:49 +1200

    faster/smarter pre-computed calc

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='gbquality',
-    version='0.20',
+    version='0.21',
     author='Andrew Lensen',
     author_email='Andrew.Lensen@ecs.vuw.ac.nz',
     license='MIT',
